c2c0f4ed07f28f71bf1dcc9bf9ec09de005e8130
banlonjones <banlonjones@go-groups.net>
2022-01-12 12:06:17 +0100

    first commit

---
+++ src/test/java/net/gogroups/blogservices/BlogServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gogroups.blogservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
