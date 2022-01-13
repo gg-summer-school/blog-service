ebb851e7b545634b9464bfba212c3f0410ffd10d
Babi-B <babibyoolah@gmail.com>
2022-01-13 10:39:17 +0100

    controller advice

--- src/main/java/net/gogroups/blogservices/swagger/SwaggerConfiguration.java
+++ src/main/java/net/gogroups/blogservices/swagger/SwaggerConfiguration.java
@@ -17,6 +17,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
+//import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -24,39 +25,50 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfiguration {
 
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-
-    public static final Contact DEFAULT_CONTACT = new Contact("www.gogroups.net", "", "someoneh@gmail.com");
-
-    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Product Catalog", "Product Catalog Description", "1.0",
-            "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Arrays.asList());
-
-    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
-            Arrays.asList("application/json"));
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+//    public static final String AUTHORIZATION_HEADER = "Authorization";
+//
+//    public static final Contact DEFAULT_CONTACT = new Contact("www.gogroups.net", "", "someoneh@gmail.com");
+//
+//    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Product Catalog", "Product Catalog Description", "1.0",
+//            "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Arrays.asList());
+//
+//    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
+//            Arrays.asList("application/json"));
+//
+//    private ApiKey apiKey() {
+//        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+//    }
+    @Configuration
+    public class SpringFoxConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("net.gogroups.blogservices"))
+                    .paths(PathSelectors.any())
+                    .build();
+        }
     }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES)
-                .consumes(DEFAULT_PRODUCES_AND_CONSUMES).securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey())).select()
-                .apis(RequestHandlerSelectors.basePackage("net.gogroups.blogservices"))
-                .paths(PathSelectors.any()).build();
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES)
+//                .consumes(DEFAULT_PRODUCES_AND_CONSUMES).securityContexts(Arrays.asList(securityContext()))
+//                .securitySchemes(Arrays.asList(apiKey())).select()
+//                .apis(RequestHandlerSelectors.basePackage("net.gogroups.blogservices"))
+//                .paths(PathSelectors.any()).build();
+//    }
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(defaultAuth()).build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-    }
+//    private SecurityContext securityContext() {
+//        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+//    }
+//
+//    private List<SecurityReference> defaultAuth() {
+//        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+//        authorizationScopes[0] = authorizationScope;
+//        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+//    }
 
 }
 
